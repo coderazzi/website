007c82f41de61282a928fa0cb62670d96a364603
coderazzi <lu@coderazzi.net>
2013-03-23 05:07:31 +0000

    Updated info on alter_table

--- python/alter_table_sqlite3/alter_table.py
+++ python/alter_table_sqlite3/alter_table.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-#       Program to remove or rename columns in a sqlite3 database
+#       Program to remove rename or reorder columns in a sqlite3 database
 #       It takes care of tables and indexes, but not of views or triggers
 #       dependencies: sqlite3
 #
@@ -44,13 +44,14 @@ def exit(msg):
 
 def showHelp():
     out=sys.stderr
-    print >> out, 'alter_table.py v0.1 (c) Coderazzi'
+    print >> out, 'alter_table.py v0.20 (c) Coderazzi'
     print >> out, 'Utility to rename or drop columns in sqlite3 tables'
     print >> out, '\narguments: filename table[:alter_op]+'
     print >> out, '       where operations are one or more of:'
     print >> out, '             table[:alter_op]+'
     print >> out, '       where alter_op can be'
     print >> out, '             -column         : drops the given column'
+    print >> out, '             column@number   : reorders the column (0...)'
     print >> out, '             column=new_name : renames a column\n'
     sys.exit(1)
 
@@ -84,7 +85,8 @@ def getDbInfo(filename):
                 columns.append(info[1].lower())
         sql = "SELECT tbl_name, sql FROM sqlite_master WHERE type='index'"
         for each in cursor.execute(sql).fetchall():
-            sql_indexes[each[0].lower()].append(beautifySql(each[1]))
+            if each[1]:
+                sql_indexes[each[0].lower()].append(beautifySql(each[1]))
         cursor.close()
         exCursor.close()
         return table_columns, sql_tables, sql_indexes
@@ -94,15 +96,28 @@ def getNewNameForColumn(current, changes):
     """
         Returns the new name for a column with the users' directives.
         If the column is to be removed, None is returned
+        @param current: the current name, case preserved.
+        @param changes: a map where the key is the current column in
+               lowercase, and the value the new column name, or None
+               to remove it
     """
     l=current.lower()
-    return changes.get(l, l)
+    return changes.get(l, current)
 
 
-def getTableChanges(sql, changes):
+def getTableChanges(sql, rowid, colRenames, colReorder):
     """
         Returns the changes in the SQL creation for a table with the 
-        given user's directives. The info is returned as a tuple:
+        given user's directives. 
+        @param sql: the sql currently used for the table creation.
+        @param rowid: new name for rowid
+        @param colRenames: a map where the key is the current column in
+               lowercase, and the value the new column name, or None
+               to remove it
+        @param colReorder: a list of tuples where the first element is
+               the current column in lowercase, and the second, the 
+               required target position, 0 based
+        The info is returned as a tuple:
         1- New SQL for that table
         2- Columns to be preserved when transfering data, 
             in the exact creation order
@@ -113,19 +128,31 @@ def getTableChanges(sql, changes):
     if not matcher:
         exit('Invalid sql:'+sql)
     sqlPrefix = matcher.group(1)
-    cols, preserve, finalNames=[], [], set()
+    cols, preserve, finalNames =[], [], []
     for each in [colMatcher.match(c) for c in matcher.group(2).split(',')]:
         if not each:
             exit('Invalid sql:'+sql)
         colName = each.group(1).strip()
-        newColName=getNewNameForColumn(colName, changes)
+        newColName=getNewNameForColumn(colName, colRenames)
         if newColName:
             lc =newColName.lower()
             if lc in finalNames:
                 exit('Column '+newColName+' would be repeated on '+sqlPrefix)
-            finalNames.add(lc)
+            finalNames.append(lc)
             preserve.append(colName)
             cols.append(newColName+' '+each.group(2).strip())
+    for col, pos in colReorder:
+        if col not in finalNames:
+            exit('Column '+col+' invalid for reordering')
+        index=finalNames.index(col)
+        if index != pos:
+            preserve.insert(pos, preserve.pop(index))
+            cols.insert(pos, cols.pop(index))
+    if rowid:
+        if rowid in finalNames:
+            exit('Column '+newColName+' would be repeated on '+sqlPrefix)
+        preserve.insert(0, 'rowid')
+        cols.insert(0, rowid+' integer primary key')
     return sqlPrefix+'('+','.join(cols)+')', preserve
 
 
@@ -133,6 +160,10 @@ def getIndexChanges(sqls, changes):
     """
         Returns the changes in the SQL creation for the indexes associated 
         to a table, with the given user's directives.
+        @param sql: the sql currently used for the index creation.
+        @param changes: a map where the key is the current column in
+               lowercase, and the value the new column name, or None
+               to remove it
     """
     ret=[]
     for sql in sqls:
@@ -152,6 +183,11 @@ def getIndexChanges(sqls, changes):
 
 
 def transfer(dbOrig, dbTarget, table, colsToPreserve):
+    """
+        Transfer the given table from database dbOrig to database dbTarget,
+        removing all columns not specified in colsToPreserve.
+        colsToPreserve must have the order defined on table creation
+    """
     def convert(value):
         if isinstance(value, unicode):
             return '"'+value+'"'
@@ -173,20 +209,24 @@ def transfer(dbOrig, dbTarget, table, colsToPreserve):
         cursor.close()
 
 
-def handle(filename, changes):
+def handle(filename, changes, reorder):
     try:
         info, sql_tables, sql_indexes=getDbInfo(filename)
     except sqlite3.DatabaseError:
         exit('Invalid sqlite3 file: %s' % filename)
-    newSql, newIndexes, colsToPreserve={}, {}, {}
+    newSql, newIndexes, colsToPreserve, rowid={}, {}, {}, None
+    pkey_check = re.compile('^.*primary\s+key.*$', re.I | re.S)
     for table, ops in changes.items():
         cols = info.get(table)
         if not cols:
             exit('No such table %s' % table)
-        for op in ops:
+        for op, val in ops.items():
             if op not in cols:
-                exit('No such column in table %s: %s' % (table, op))
-        sql, keep = getTableChanges(sql_tables[table], ops)
+                if op=='rowid' and not pkey_check.match(sql_tables[table]):
+                    rowid=ops.pop(op)
+                else:
+                    exit('No such column in table %s: %s' % (table, op))
+        sql, keep = getTableChanges(sql_tables[table], rowid, ops, reorder)
         newIndexes[table]=getIndexChanges(sql_indexes[table], ops)
         newSql[table]=sql
         colsToPreserve[table]=keep
@@ -208,14 +248,14 @@ def handle(filename, changes):
                 
 
 if __name__=='__main__':
-    changes, filename = {}, sys.argv[1]
+    reorder, changes, filename = [], {}, sys.argv[1]
     if len(sys.argv)<3 or filename[0]=='-':
         showHelp()
     if not os.path.exists(filename) or os.path.isdir(filename):
         exit('Invalid sqlite3 file: %s' % filename)
     opArgs = sys.argv[2:]
     tableOp = re.compile('([^\:\s]+)\:(.+)$')
-    basicOp = re.compile('^(?:-(.+))|(?:([^=]+)=(.+))$')
+    basicOp = re.compile('^(?:-(.+))|(?:([^=]+)=([a-zA-Z].*))|(?:([^@]+)@(\d+))$')
     for arg in opArgs:
         match = tableOp.match(arg)
         if not match:
@@ -229,9 +269,13 @@ if __name__=='__main__':
             match=basicOp.match(each)
             if not match:
                 exit('Invalid operation: '+arg)
-            column, value = match.group(1), None
-            if not column:
-                column = match.group(2)
-                value = match.group(3) 
-            opTable[column.lower()]=value
-    handle(filename, changes)
+            column = match.group(4)
+            if column:
+                reorder.append((column.lower(), int(match.group(5))))
+            else:
+                column, value = match.group(1), None
+                if not column:
+                    column = match.group(2)
+                    value = match.group(3) 
+                opTable[column.lower()]=value
+    handle(filename, changes, reorder)
