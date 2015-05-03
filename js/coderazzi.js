942eab567125b217bb5d982669aa3cd72c369e1e
coderazzi <lu@coderazzi.net>
2015-05-03 22:03:15 +0000

    Responsive menu in place

--- js/coderazzi.js
+++ js/coderazzi.js
@@ -1,3 +1,4 @@
+//google analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -5,3 +6,35 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-16271101-1', 'auto');
 ga('send', 'pageview');
+
+
+//responsive menu
+var menu=document.getElementById('menu');
+
+var nav=menu.getElementsByClassName('navcontainer')[0];
+
+menu.onclick=function(){
+	if (nav.offsetParent===null){ //navcontainer is hidden
+		nav.style.display='block';
+	} else {
+		hideResponsiveMenu();
+	}
+};
+
+function getStyle(element, prop){
+	if(window.getComputedStyle) return getComputedStyle(element).getPropertyValue(prop);
+	try { return element.currentStyle[prop];} 
+	catch (e) { return ''; }
+}
+
+function hideResponsiveMenu(){
+	if (getStyle(menu, 'float')==='none'){ //not hidden, but perhaps the menu is not on responsive mode		
+		nav.style.display='none';
+	}	
+}
+
+window.onkeydown = function( event ) {
+    if (event.keyCode===27) {
+    	hideResponsiveMenu();
+    }
+};
