136e1afd1382953a6cdb0c0c2fe83fe885b8ba8d
coderazzi <lu@coderazzi.net>
2020-05-09 16:47:26 +0200

    Added script to push changes to coderazzi.net

---
+++ push_changes.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+host=coderazzi.net
+user='coderazzi'
+flocal=/home/coderazzi/development/website/website/
+fremote='/home/coderazzi/www/coderazzi.net/public/'
+
+COMMAND="rsync --delete -acvrz -e 'ssh -l ${user}' ${flocal} ${host}:\"${fremote}\" "
+echo $COMMAND
+eval $COMMAND
