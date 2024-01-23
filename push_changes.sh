43ba191e37c20eac1116743f6e381f795c96555a
coderazzi <lu@coderazzi.net>
2024-01-23 19:25:45 +0000

    push_changes.sh usable in Mac and Linux

--- push_changes.sh
+++ push_changes.sh
@@ -4,6 +4,9 @@
 host=coderazzi.net
 user='coderazzi'
 flocal=/Users/coderazzi/development/website/website/
+if [ ! -d "$flocal" ]; then
+  flocal=/home/coderazzi/development/website/website/
+fi
 fremote='/home/coderazzi/www/coderazzi.net/public/'
 
 COMMAND="rsync --delete -acvrz -e 'ssh -l ${user}' ${flocal} ${host}:\"${fremote}\" "
