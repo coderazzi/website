6f7af546de9279d65c84f57d75c1a69da42bd956
coderazzi <coderazzi@gmail.com>
2025-01-07 14:58:22 +0100

    Extending push_changes to address gihub

--- push_changes.sh
+++ push_changes.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 
+die () {
+  echo "$*"
+  exit 1
+}
 
-host=coderazzi.net
-user='coderazzi'
-flocal=/Users/coderazzi/development/website/website/
-if [ ! -d "$flocal" ]; then
-  flocal=/home/coderazzi/development/website/website/
+LOCAL=/Users/coderazzi/development/coderazzi.github.io/
+if [ ! -d "$LOCAL" ]; then
+  LOCAL=/home/coderazzi/development/coderazzi.github.io/
 fi
-fremote='/home/coderazzi/www/coderazzi.net/public/'
 
-COMMAND="rsync --delete -acvrz -e 'ssh -l ${user}' ${flocal} ${host}:\"${fremote}\" "
-echo $COMMAND
-eval $COMMAND
+cd ${LOCAL} 2> /dev/null || die "Cannot access $LOCAL"
+
+if  false ; then
+  # push to own server
+  host=coderazzi.net
+  user='coderazzi'
+  REMOTE='/home/coderazzi/www/coderazzi.net/public/'
+
+  COMMAND="rsync --delete -acvrz --exclude .git -e 'ssh -l ${user}' . ${host}:\"${REMOTE}\" "
+  echo "$COMMAND"
+  eval "$COMMAND"
+fi
+
+echo GOING
+DATE=$(git log  2> /dev/null | head -n 3 | tail -n 1 | cut -d: -f2-)
+[ -z "$DATE" ] && die "$LOCAL is not in git, unexpected, cannot continue"
+
+cd ../website || die "cannot access ${LOCAL}/../website"
+[ -z "$(git status --porcelain)" ] || die "There are uncommited changes in $(pwd) folder"
+
+
