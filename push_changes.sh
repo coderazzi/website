4e1b920b03363def7af836f371ab874d062c6fb5
coderazzi <coderazzi@gmail.com>
2025-01-07 15:08:06 +0100

    Extended push_changes to address gihub

--- push_changes.sh
+++ push_changes.sh
@@ -23,11 +23,18 @@ if  false ; then
   eval "$COMMAND"
 fi
 
-echo GOING
+[ -z "$(git status --porcelain)" ] && die "There are no uncommited changes in $(pwd) folder"
 DATE=$(git log  2> /dev/null | head -n 3 | tail -n 1 | cut -d: -f2-)
 [ -z "$DATE" ] && die "$LOCAL is not in git, unexpected, cannot continue"
 
 cd ../website || die "cannot access ${LOCAL}/../website"
 [ -z "$(git status --porcelain)" ] || die "There are uncommited changes in $(pwd) folder"
 
+MESSAGE=$(git log --since="$DATE" --pretty=%s  | sort -u | paste -sd "|" -)
+[ -z "$MESSAGE" ] && echo No message in commit folder, nothing commited lately in $LOCAL
 
+cd ${LOCAL}
+git add -A
+git commit -m "$MESSAGE"
+git push
+echo Changes pushed, commited with message $MESSAGE
